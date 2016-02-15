04bf18dd27e326c99b0dfe75478b85de03914eb9
jonsth131 <jonsth131@studentmail.oru.se>
2016-02-15 22:05:06 +0100

    Switched ZAP-api to NZap.

--- Securitytests.cs
+++ Securitytests.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 using NUnit.Framework;
+using NZap;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Remote;
-using OWASPZAPDotNetAPI;
 
 namespace Securitytesting
 {
@@ -11,11 +13,11 @@ namespace Securitytesting
     {
         private const string Proxy = "localhost";
         private const int ProxyPort = 8080;
-        private const string Target = "http://localhost";
+        private const string Target = "http://localhost/";
+        private const string ApiKey = "b56i96opec68mts9ob05pdpigg";
 
-        private FirefoxDriver driver;
-        private ClientApi client;
-        private static IApiResponse _apiResponse;
+        private FirefoxDriver _driver;
+        private IZapClient _client;
 
         [SetUp]
         public void SetUp()
@@ -28,35 +30,83 @@ namespace Securitytesting
             };
             var desiredCapabilities = new DesiredCapabilities();
             desiredCapabilities.SetCapability(CapabilityType.Proxy, proxy);
-            driver = new FirefoxDriver(desiredCapabilities);
-            driver.Manage().Timeouts().ImplicitlyWait(new TimeSpan(0, 0, 30));
-            client = new ClientApi(Proxy, ProxyPort);
+            _driver = new FirefoxDriver(desiredCapabilities);
+            _driver.Manage().Timeouts().ImplicitlyWait(new TimeSpan(0, 0, 30));
+            _client = new ZapClient(Proxy, ProxyPort);
+            var parameters = new Dictionary<string, string> { { "apikey", ApiKey }, { "site", Target } };
+            _client.CallApi("httpSessions", "action", "createEmptySession", parameters);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Dispose();
+            //_client.Dispose();
         }
 
         [Test]
         public void TestMethod1()
         {
-            driver.Navigate().GoToUrl(Target);
+            _client.Pscan.EnableAllScanners(ApiKey);
+            _driver.Navigate().GoToUrl(Target);
             PrintAlertsToConsole();
-            driver.Dispose();
+        }
+
+        [Test]
+        public void VulnerabilityScanBeforeLogin()
+        {
+            _driver.Navigate().GoToUrl(Target);
+            var parameters = new Dictionary<string, string> { { "apikey", ApiKey }, { "url", Target }, { "maxChildren", "5" }, { "recurse", "5" } };
+            var apiResponse = _client.CallApi("spider", "action", "scan", parameters);
+            var value = apiResponse.Value;
+            var complete = 0;
+            while (complete < 100)
+            {
+                var params2 = new Dictionary<string, string> { { "scanId", value } };
+                var callApi = _client.CallApi("spider", "view", "status", params2);
+                complete = int.Parse(callApi.Value);
+                Thread.Sleep(1000);
+            }
+            PrintAlertsToConsole();
+        }
+
+        [Test]
+        public void VulnerabilityScanAfterLogin()
+        {
+            _driver.Navigate().GoToUrl(Target);
+            _driver.FindElementByName("username").SendKeys("admin");
+            _driver.FindElementByName("password").SendKeys("password");
+            _driver.FindElementByName("Login").Click();
+            var parameters = new Dictionary<string, string> { { "apikey", ApiKey }, { "url", Target }, {"recurse", "5"} };
+            var apiResponse = _client.CallApi("ascan", "action", "scan", parameters);
+            var value = apiResponse.Value;
+            var complete = 0;
+            while (complete < 100)
+            {
+                var params2 = new Dictionary<string, string> { { "scanId", value } };
+                var callApi = _client.CallApi("ascan", "view", "status", params2);
+                complete = Int32.Parse(callApi.Value);
+                Thread.Sleep(1000);
+            }
+            PrintAlertsToConsole();
         }
 
         private void PrintAlertsToConsole()
         {
-            var alerts = client.GetAlerts(Target, 0, 0);
+            var alerts = _client.GetAlerts(Target);
             foreach (var alert in alerts)
             {
-                Console.WriteLine(alert.AlertMessage
+                Console.WriteLine(alert.Alert
                     + Environment.NewLine
-                    + alert.CWEId
+                    + alert.Cweid
                     + Environment.NewLine
                     + alert.Url
                     + Environment.NewLine
-                    + alert.WASCId
+                    + alert.Wascid
                     + Environment.NewLine
                     + alert.Evidence
                     + Environment.NewLine
-                    + alert.Parameter
+                    + alert.Param
                     + Environment.NewLine
                 );
             }
